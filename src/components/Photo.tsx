55117c0c503be32b4d60a498d302db8b8f29aafb
TusharRathi108 <tushar.rathi.tech@gmail.com>
2024-06-17 06:38:03 +0530

    Implemented photo and typewriter effect

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,12 +1,61 @@
-import { AvatarImage } from "@radix-ui/react-avatar";
-import { Avatar } from "./ui/avatar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { motion } from "framer-motion";
 
 const Photo = () => {
   return (
-    <div className="h-full w-full flex items-center relative">
-      <Avatar className="w-[250px] h-[225px] md:w-[300px] md:h-[300px] xl:w-[458px] xl:h-[448px]">
-        <AvatarImage src="src/assets/Profile1-removebg.png"/>
-      </Avatar>
+    <div className="h-full w-full relative">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
+      >
+        {/* Profile image */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] absolute"
+        >
+          <Avatar className="h-full w-full">
+            <AvatarImage
+              src="src/assets/Profile1-removebg.png"
+              style={{ objectFit: "scale-down" }}
+            />
+          </Avatar>
+        </motion.div>
+
+        {/* Circle */}
+        <motion.svg
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 10,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
+      </motion.div>
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,51 +2,69 @@ import Photo from "@/components/Photo";
 import Socials from "@/components/Socials";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
+import { Typewriter } from "react-simple-typewriter";
+
+// typewriter words array.
+const skills = [
+  "Frontend: React, Next.js, Tailwind CSS, Radix UI, Chakra UI",
+  "Backend: Node.js, Express, Prisma, Git HUB",
+  "Database: MongoDB Atlas, MySQL",
+  "Tools & Libraries: Shadcn/UI, Vite, TypeScript, Axios, Tanstack Query, React Router, Recharts",
+];
 
 const HomePage = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col md:flex-row items-center justify-between md:pt-8 md:pb-20">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-[81px]">
           {/* Information about me */}
-          <div className="text-center md:text-left mt-5 order-2 md:order-none">
-            <span className="text-xl"> Software Developer </span>
-            <h1 className="h3 md:h2 xl:h1 mt-8 mb-8 md:mb-9">
-              {" "}
-              Hello I'm <br />{" "}
-              <span className="text-accent"> Tushar Rathi </span>{" "}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-2xl block xl:inline">
+              Full-Stack Developer
+            </span>
+            <h1 className="h1 mb-3">
+              Hello I'm!
+              <br />
+              <span className="text-accent">Tushar Rathi</span>
             </h1>
-            <p className="hidden md:block max-w-[500px] mb-12 md:mb-9 text-white/80">
-              {" "}
-              I excel at crafting elegant digital experiences and I am
-              proeficient in various programming languages and technologies.{" "}
+            <p className="max-w-[500px] mx-auto xl:mx-0 mb-9 text-white/80">
+              A passionate developer with a knack for crafting
+              elegant digital experiences. Proficient in:{" "}
+              <br/>
+              <span className="text-accent">
+                {"  "}
+                <Typewriter
+                  words={skills}
+                  loop={0}
+                  cursor
+                  cursorStyle="_"
+                  typeSpeed={200}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />{" "}
+              </span>
             </p>
             {/* socials */}
-            <div className="flex flex-col md:flex-row items-center gap-10 md:gap-9">
+            <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
                 variant="outline"
                 size={"lg"}
-                className="flex items-center gap-2 transition delay-150 duration-300 ease-in-out"
+                className="uppercase flex items-center gap-2 transition delay-150 duration-300 ease-in-out"
               >
-                {" "}
-                <span>
-                  {" "}
-                  Download CV{" "}
-                </span> <FiDownload className="text-xl" />{" "}
+                <span>Download CV</span> <FiDownload className="text-xl" />
               </Button>
-              <div className="mb-5 md:mb-0">
-                {" "}
+              <div className="mb-3 xl:mb-0">
                 <Socials
                   containerStyles={"flex gap-6 "}
                   iconStyles={
                     "w-12 h-12 border border-accent rounded-full flex justify-center items-center text-accent hover:bg-accent hover:text-primary hover:transition-all duration-500"
                   }
-                />{" "}
+                />
               </div>
             </div>
           </div>
           {/* Profile photo */}
-          <div className="mb-2 order-1 md:order-none">
+          <div className="mb-5 order-1 xl:order-none">
             <Photo />
           </div>
         </div>
