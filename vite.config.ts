9f4c10f2484ea3ea48fe0950413c9b3e9ab31362
TusharRathi108 <tushar.rathi.tech@gmail.com>
2024-06-21 10:15:35 +0530

    Adjusted chunk

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,20 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+  },
 });
